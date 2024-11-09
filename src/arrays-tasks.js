ce4bc8ea55def7839b026b82aa7504c065215d80
Aleksei Zhuchkov <kelzzzer@gmail.com>
2024-11-09 21:15:37 +0300

    feat: complete task sumArrays and getIntervalArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,8 +20,10 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  return Array.from({ length: end - start + 1 })
+    .fill(start)
+    .map((el, ind) => el + ind);
 }
 
 /**
@@ -37,8 +39,13 @@ function getIntervalArray(/* start, end */) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const maxLength = Math.max(arr1.length, arr2.length);
+  return Array.from({ length: maxLength }).map((el, ind) => {
+    const num1 = arr1[ind] ? arr1[ind] : 0;
+    const num2 = arr2[ind] ? arr2[ind] : 0;
+    return num1 + num2;
+  });
 }
 
 /**
