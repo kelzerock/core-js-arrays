7d0a4786fc1147bdd05577d8c0f35be9da6f30b3
Aleksei Zhuchkov <kelzzzer@gmail.com>
2024-11-11 00:10:11 +0300

    feat shiftArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -615,8 +615,10 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  const position = n > 0 ? arr.length - n : 0;
+  const halfArr = arr.splice(position, Math.abs(n));
+  return position ? [...halfArr, ...arr] : [...arr, ...halfArr];
 }
 
 /**
